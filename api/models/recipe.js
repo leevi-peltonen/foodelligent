b3580eea5c789d4783e41c1fd829f33a996d1e8a
Eppu Peltonen <eppu.peltonen@icloud.com>
2022-08-25 20:59:18 +0300

    add user reference to recipe schema

--- api/models/recipe.js
+++ api/models/recipe.js
@@ -6,7 +6,19 @@ const recipeSchema = new mongoose.Schema({
     steps: [String],
     rating: Number,
     time: Number,
-    timeRateIndex: Number
+    timeRateIndex: Number,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
+
+recipeSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
 
 module.exports = mongoose.model('Recipe', recipeSchema);
